bb4fd9dc62e1005df6bd8c9021c97a825ac328c6
Juan Ballesteros <juanballesteros2001@gmail.com>
2025-08-16 18:30:56 -0500

    Add: implement environment configuration and HTTP client
    What: Added environment loading functionality in `env/main.go`, created a shared HTTP client in `shared/client.go`, and introduced a sample `.env.example` file. Updated `tracker/main.go` to utilize the new environment variable for the base transaction URL and implemented email storage logic with retry mechanism.
    
    Why: Enhance configuration management and improve the transaction tracker application by enabling dynamic URL handling and robust email storage functionality.

---
+++ shared/client.go
@@ -0,0 +1,7 @@
+package shared
+
+import "net/http"
+
+var (
+	Client = http.Client{}
+)
--- tracker/main.go
+++ tracker/main.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
+	"os"
+	"time"
 
+	_ "transaction-tracker/env"
 	"transaction-tracker/googleapi"
+	"transaction-tracker/shared"
 )
 
 const (
@@ -14,18 +21,82 @@ const (
 	topic        = "gmail-notifications"
 )
 
+type Message struct {
+	EmailAdress string `json:"emailAddress"`
+	HistoryID   uint64 `json:"historyId"`
+}
+
+const (
+	STORE_EMAIL_MAX_RETRIES = 5
+)
+
+var (
+	baseURL       = os.Getenv("BASE_TRANSACTION_URL")
+	urlStoreEmail = "/api/v1/gmail/emails/%d/save"
+)
+
+func storeEmail(message *Message, maxRetries int) error {
+	data, err := json.Marshal(message)
+	if err != nil {
+		return fmt.Errorf("Error marshalling message: %v", err)
+	}
+
+	req, err := http.NewRequest("POST", baseURL+fmt.Sprintf(urlStoreEmail, message.HistoryID), bytes.NewBuffer(data))
+	if err != nil {
+		return fmt.Errorf("Error creating request: %v", err)
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+
+	res, err := shared.Client.Do(req)
+	if err != nil {
+		return err
+	}
+
+	defer res.Body.Close()
+
+	if res.StatusCode == 404 && maxRetries > 0 {
+		time.Sleep(5 * time.Second)
+		return storeEmail(message, maxRetries-1)
+	}
+
+	if res.StatusCode != 200 {
+		return fmt.Errorf("Error unexpected status code: %d", res.StatusCode)
+	}
+
+	return nil
+}
+
 func main() {
 	ctx := context.Background()
+
+	if urlStoreEmail == "" {
+		log.Fatal("missing url to store the email")
+	}
 
 	pubsubService, err := googleapi.NewGooglePubSub(ctx, projectID)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println("Esperando mensajes...")
-
 	err = pubsubService.Subscribe(ctx, subscription, func(ctx context.Context, msg []byte) error {
-		fmt.Println("Mensaje recibido:", string(msg))
+		message := &Message{}
+		err := json.Unmarshal(msg, message)
+		if err != nil {
+			return err
+		}
+
+		log.Printf("event: message_received, email: %s, history_id: %d", message.EmailAdress, message.HistoryID)
+
+		err = storeEmail(message, STORE_EMAIL_MAX_RETRIES)
+		if err != nil {
+			log.Print("Error storing email:", err)
+
+			return nil
+		}
+
+		log.Printf("event: message_stored, email: %s, history_id: %d", message.EmailAdress, message.HistoryID)
+
 		return nil
 	})
 
